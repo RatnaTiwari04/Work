ff6e7a62f6919af03e72b5252868c0eb421c7942
unknown <ratantiwary04@gmail.com>
2025-07-07 09:03:37 +0530

    Your commit message

--- src/pages/Example2.tsx
+++ src/pages/Example2.tsx
@@ -1,29 +1,39 @@
-import React, { useState} from "react";
-import TextInput from "../components/TextInput";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import TextInput from "../components/TextInput";
 
 function Example2() {
-    const [internal, setinternal] = useState("");
-    const navigate = useNavigate();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const navigate=useNavigate();
+
   return (
     <div className="form-showcase">
-      <h2>TextInput Internal label Form</h2>
-      <div className="form-group">
-        <h3>Internal</h3>
+      <h2>Two Column internal Label Form</h2>
+      <div className="form-row">
         <TextInput
-        label="Label"
-          value={internal}
-          onChange={setinternal}
-          placeholder="Place holder"
-          labelPosition="internal"/>
-        </div>
-        <div >
+          label="First name"
+          value={firstName}
+          onChange={setFirstName}
+          placeholder="First name"
+          labelPosition="internal"
+        />
+        <TextInput
+          label="Last name"
+          value={lastName}
+          onChange={setLastName}
+          placeholder="Last name"
+          labelPosition="internal"
+        />
+      </div>
+      <div >
         <button onClick={() => navigate("/Example3")} className="btn">
           Go to Example 3
         </button>
       </div>
-        </div>
-  )
+    </div>
+  );
 }
 
 export default Example2
+
