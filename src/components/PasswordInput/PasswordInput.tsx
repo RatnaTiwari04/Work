d17421957a249958e9a33707a8ebfc315f7319d3
unknown <ratantiwary04@gmail.com>
2025-07-08 17:36:43 +0530

    add

--- src/components/PasswordInput/PasswordInput.tsx
+++ src/components/PasswordInput/PasswordInput.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { Eye, EyeOff, AlertCircle, Lock } from "lucide-react";
 import "./PasswordInput.scss";
 
